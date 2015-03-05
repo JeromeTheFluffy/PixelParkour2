c760b4225367aad86fcf129ddd72c7d7fafc7550
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-03-05 10:50:44 -0700

    Create colours.py

---
+++ colours.py
@@ -0,0 +1,15 @@
+import pygame, sys
+pygame.init()
+
+#################################################
+#colour variables                               #
+#################################################
+
+white(255,255,255)
+black(0,0,0)
+brightGreen(0,255,0)
+brightRed(255,0,0)
+brightBlue(0,0,255)
+lightBlue(128,128,255)
+lightGreen(128,255,128)
+lightRed(255,128,128)
