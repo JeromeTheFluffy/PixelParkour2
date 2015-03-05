eec04433772ddcb2ca6cc477da2b2298748358d5
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-03-05 11:30:42 -0700

    Create load.py

---
+++ load.py
@@ -0,0 +1,22 @@
+import pygame, sys, time
+pygame.init()
+
+###########################################
+#loads all images with the load() function#
+###########################################
+def load():
+  pygame.load.image('runner1.png')
+  pygame.load.image('runner2.png')
+  pygame.load.image('runner.png3')
+  pygame.load.image('runner.png4')
+  pygame.load.image('runner.png5')
+  pygame.load.image('runner.png6')
+  pygame.load.image('runner.png7')
+  pygame.load.image('runner.png8')
+  pygame.load.image('runner.png9')
+  pygame.load.image('runner.png10')
+  pygame.load.sound('theme1.mp3')
+  pygame.load.sound('theme2.mp3')
+  pygame.load.sound('theme3.mp3')
+  pygame.load.sound('theme4.mp3')
+  #put other files here##################################
